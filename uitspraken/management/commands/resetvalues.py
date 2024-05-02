7ab290328cc1ed4ddfd92410ec4a0d0e9bf38a1c
jeroenvandenieuwenhof <jeroen@vandenieuwenhof.nl>
2024-05-02 16:33:21 +0200

    Add resetvalues management command

---
+++ uitspraken/management/commands/resetvalues.py
@@ -0,0 +1,22 @@
+from uitspraken.models import Uitspraak
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Reset values for Uitspraken"
+
+    def handle(self, *args, **options):
+        uitspraken = Uitspraak.objects.all()
+        for uitspraak in uitspraken:
+            uitspraak.beslissing = ""
+            uitspraak.appellant = ""
+            uitspraak.counterpart = ""
+            uitspraak.oordeel = 0
+            uitspraak.plaats = ""
+            uitspraak.provincie = ""
+            uitspraak.label = "NEW"
+            uitspraak.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Successfully reset values for uitspraken")
+        )
